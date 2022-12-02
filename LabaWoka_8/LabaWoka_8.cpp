8e11981a2572a8ae90c18886c02efb114a824fa3
SerGun0k <113134004+SerGun0k@users.noreply.github.com>
2022-12-02 20:16:37 +0200

    Add files via upload

---
+++ LabaWoka_8/LabaWoka_8.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+int main()
+{
+	long long cur;
+	std::cin >> cur;
+
+
+	for (int i = 0; i <= sizeof(cur); i++)
+	{
+		printf("%d ", *(((unsigned char*)&cur) + i));
+	}
+
+	std::cout << std::endl;
+
+	// from −9223372036854775808 to 9223372036854775807 и занимает 8 байт
+
+
+	
+
+
+
+}
+
+
